209b43eefd6862599ab1d2f56bcd899a4283ac2f
tddebart <tddebart@gmail.com>
2021-09-06 11:06:18 +0200

    berekeningen met variables

---
+++ feestlunch.py
@@ -0,0 +1,9 @@
+croissantHoeveelheid = 17
+croissantPrijs = 0.39
+stockbroodHoeveelheid = 2
+stockbroodPrijs = 2.78
+bonnenHoeveelheid = 3
+bonnenKorting = 0.50
+
+print(croissantHoeveelheid*croissantPrijs+stockbroodHoeveelheid*stockbroodPrijs-bonnenHoeveelheid*bonnenKorting)
+
---
+++ speelhal-dag.py
@@ -0,0 +1,6 @@
+toegangsTicketPrijs = 7.45
+aantalPersonen = 3
+lengteVR = 45
+vrPrijs = 0.37
+
+print(aantalPersonen*toegangsTicketPrijs+(lengteVR/5)*vrPrijs*aantalPersonen)
