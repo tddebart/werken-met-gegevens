482708537c7eba92449995077f9ba563b91cbde9
tddebart <tddebart@gmail.com>
2021-09-06 11:20:36 +0200

    betere prints in the calculations

--- feestlunch.py
+++ feestlunch.py
@@ -5,5 +5,8 @@ stockbroodPrijs = 2.78
 bonnenHoeveelheid = 3
 bonnenKorting = 0.50
 
-print(croissantHoeveelheid*croissantPrijs+stockbroodHoeveelheid*stockbroodPrijs-bonnenHoeveelheid*bonnenKorting)
+volledigePrijs = (croissantHoeveelheid*croissantPrijs+stockbroodHoeveelheid*stockbroodPrijs-bonnenHoeveelheid*bonnenKorting)
+
+print("De feestlunch kost je bij de bakker " + str(volledigePrijs) + " euro voor de " + str(croissantHoeveelheid) + " croissantjes en de " + str(stockbroodHoeveelheid) + " stokbroden als de " + str(bonnenHoeveelheid) + " kortingsbonnen nog geldig zijn!")
+print(volledigePrijs)
 
--- speelhal-dag.py
+++ speelhal-dag.py
@@ -3,4 +3,7 @@ aantalPersonen = 3
 lengteVR = 45
 vrPrijs = 0.37
 
-print(aantalPersonen*toegangsTicketPrijs+(lengteVR/5)*vrPrijs*aantalPersonen)
+volledigePrijs = aantalPersonen*toegangsTicketPrijs+(lengteVR/5)*vrPrijs*aantalPersonen
+
+print("Dit geweldige dagje-uit met {} mensen in de Speelhal met {} minuten VR kost je maar {} euro".format(aantalPersonen, lengteVR, volledigePrijs))
+print(volledigePrijs)
